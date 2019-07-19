413b5736b9c3eb0b54767629655311bf27416d7f
masako123 <h.masako123@gmail.com>
2019-07-19 15:18:59 +0900

    Complete create janken2

--- janken_games_2.rb
+++ janken_games_2.rb
@@ -1,110 +1,106 @@
-
-# オブジェクト指向
-
-# じゃんけんのルールを定義
-class Rule
+class Janken
     HANDS = [:rock, :scissors, :paper]
-    def hands_list
-        hash = {}
-        hash[:rock] = { win: "scissors", loose: "paper", even: "rock" }
-        hash[:scissors] = { win: "paper", loose: "rock", even: "scissors" }
-        hash[:paper] = { win: "rock", loose: "scissors", even: "paper" }
-        hash
+  
+    def initialize
+        puts "こんにちは、じゃんけんしましょう。"
+        puts "最初はグー、じゃんけん、"
+        puts "数字を選んでください  >>>>"
     end
-    def win_choice(hand)
-        hands_list[hand][:win]
-    end
-end
-
-# じゃんけんをする登場人物の定義
-class Player
-    attr_reader :name
-    def initialize(name)
-        @name = name
-    end
-end
-
-# じゃんけんを実施することを定義
-class Event
-    include Rule
-    def initialize(player, cpu)
-        @player = player
-        @cpu = cpu
-
-        # puts "Hello #{@player}san, let's play Janken."
-        # play_start
-    end
-
-    def play_start
-        puts "Rock, Scissors, Paper, GO!"
-        puts "Select by index number from 1 to 3  >>>>"
-    end
-
-    def show_hands
-        index = 1
-        HANDS.each do |x|
-            puts "#{x}:#{index}"
-            index += 1
+  
+    def display_hand_text(hand)
+        case hand
+        when :rock
+            "グー"
+        when :scissors
+            "チョキ"
+        when :paper
+            "パー"
         end
-        # puts "Rock:1"
-        # puts "Scissors:2"
-        # puts "Paper:3"
-    end
-
-    def player_input
-        @number = gets.chomp.to_i - 1 
-    end
-
-    def player_output
-        HANDS[@number]
-    end
-
-    def cpu_output
-        HANDS.sample
-    end
-
-    def players_output
-        [player_output, cpu_output]
-        puts "#{@player}san showed #{player_output}, #{@cpu} showed #{cpu_output}"
     end
     
-    def judge
-        if player_output == win_choice(player_output)?
-            puts "YOU WIIIIIIN!"
-        elsif player_output == cpu_output
-            puts "Even. Play again."
-            play_start
-        elsif @number != 1 || 2 || 3
-            puts "Oops, you've mistyped the number."
-            play_start
+    # インデント付きでじゃんけんの手札を表示する
+    def display_hands
+        HANDS.each_with_index do |hand, i|  
+            puts "#{display_hand_text(hand)}:#{i+1}"
+        end
+    end 
+
+    # 入力値を定義し、入力値を間違うと繰り返すことも定義
+    def player_input
+      display_hands
+      @player_hand_number = gets.chomp.to_i - 1
+      result = correct_input?(@player_hand_number)
+      player_input unless result
+    end
+    
+    def player_output
+      HANDS[@player_hand_number]
+    end
+  
+    def cpu_player_input
+      @item = HANDS.sample
+    end
+  
+    def cpu_player_output
+      @item
+    end
+  
+    def players_output
+      [player_output, cpu_player_output]
+    end
+  
+    def display_players_output
+      puts "あなたは#{display_hand_text(player_output)}, 私は#{display_hand_text(cpu_player_output)}"
+    end
+    
+    # 勝ちパターン
+    def hands_win_list
+        hash = {}
+        hash[:rock] = :scissors
+        hash[:scissors] = :paper
+        hash[:paper] = :rock
+        hash
+    end
+
+    def player_win_hand
+        hands_win_list[player_output]
+      end
+
+    # アイコかどうか判定するメソッド
+    def even?
+        players_output.uniq.count == 1
+    end
+    # 入力値が正しいかどうか判定するメソッド
+    def correct_input?(input)
+        if [0,1,2].include?(input)
+          true
         else
-            puts "YOU LOOOOOOSE"
+          puts "おっと、入力した値が間違っています。"
+          false
         end
     end
+
+    def display_try_again
+      puts "もう一回。アイコ　でしょ！"
+    end
+  
+    def judge
+        display_players_output
+        while even? do
+            display_try_again
+            player_input
+            display_players_output
+        end
+        if player_win_hand == cpu_player_output
+            puts "YOU WIIIIIIN!"
+        else
+            puts "YOU LOOOOOOSE"
+        end    
+    end   
 end
 
 
-
-player = Player.new("masako")
-player2 = Player.new("cpu")
-event = Event.new(player, player2)
-
-event.play_start
-event.show_hands_list
-event.player_input
-player = event.player_output
-event.cpu_input
-cpu = event.cpu_output
-winner = event.judge
-
-event.judge
-event.show_result
-event.winner?
-
-
-
-
-
-
-
-
+janken = Janken.new
+janken.player_input
+janken.cpu_player_input
+janken.judge
